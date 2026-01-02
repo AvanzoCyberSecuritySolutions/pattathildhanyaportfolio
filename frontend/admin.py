54cba9c461c133be3b153f8a16bb2efcde263665
athira <athira21041999@gmail.com>
2026-01-02 13:41:04 +0530

    changes in admin

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import HomeGalleryImage
+
+@admin.register(HomeGalleryImage)
+class HomeGalleryImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'uploaded_at')
